508d0adf0e44f7c4bed4cc5bab71ce4da5304069
mehdi-romdhani <mehdi.romdhani@laplateforme.io>
2023-02-27 11:41:26 +0100

    initilisation CSS

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link Css -->
+    <link rel="stylesheet" href="./assets/style.css">
+    <!-- Link Js -->
+    <script defer src="./assets/js/script.js"></script>
+    <!-- API FONT GOOGLE -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
+    <!-- Title Document -->
+    <title>Idle_game</title>
+</head>
+
+<body>
+
+
+    <div class="main-container">
+        <h1>IDLE GAME_</h1>
+
+    </div>
+
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">H O M <span style="color:brown;">E</span> </a></li>
+
+            </ul>
+        </nav>
+    </header>
+
+    <div class="second-container">
+        <div class="btn-container">
+            <button class="fill">Inscription</button>
+            <button class="fill">Connexion</button>
+        </div>
+    </div>
+</body>
+
+</html>
